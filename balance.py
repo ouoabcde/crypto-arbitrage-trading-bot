75b09937c9c3ba4c5631d78afe360138f89d9da2
천정현 <ouoabcde@naver.com>
2023-10-10 15:25:28 +0900

    Add fetch ohlcv 1d, check balance file

---
+++ balance.py
@@ -0,0 +1,16 @@
+import ccxt
+import pprint
+
+with open("./upbit.key") as f:
+    lines = f.readlines()
+    api_key = lines[0].strip()
+    api_secret = lines[1].strip()
+
+exchange = ccxt.upbit(config={
+    'apiKey': api_key,
+    'secret': api_secret,
+    'enableRateLimit': True
+})
+
+balance = exchange.fetch_balance()
+pprint.pprint(balance)
